71e573732109b26b018f81b48cac85edebd91e78
Blake Scala and Garrett Boggs <blake@>
2016-09-08 09:45:27 -0700

    finish program

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,5 +3,15 @@ import java.io.Console;
 public class App {
   public static void main(String[] args) {
     Console console = System.console();
+
+    System.out.println("Enter one side length");
+    int sideOne = Integer.parseInt(console.readLine());
+
+    System.out.println("Enter another side length");
+    int sideTwo = Integer.parseInt(console.readLine());
+
+    Rectangle rectangle = new Rectangle(sideOne,sideTwo);
+
+    System.out.println("Is your rectangle a square. too?" + rectangle.isSquare());
   }
 }
